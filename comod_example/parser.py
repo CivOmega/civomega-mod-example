7dbdc75e2779f89663e2f0ffea056a7de2af4634
Mike Tigas <mike@tig.as>
2014-04-21 22:16:42 -0400

    kwarg-based parser

--- comod_example/parser.py
+++ comod_example/parser.py
@@ -10,29 +10,44 @@ from .patterns import PATTERNS
 import json
 from django.template import loader, Context
 from random import Random
+import re
+
+PATTERN_ARGS_RE = re.compile(r'{([A-Za-z0-9_]+)}')
+
 
 
 ############################################################
 # Pattern-dependent behavior
-def answer_pattern(pattern, args):
+def answer_pattern(pattern_str, args):
     """
     Returns a `dict` representing the answer to the given
     pattern & pattern args.
     """
-    if pattern not in PATTERNS:
+    if pattern_str not in PATTERNS:
       return None
     if len(args) != 1:
       return None
 
-    name = args[0]
+    # dumb example of how to get key-value pairs back,
+    # by matching the positional args with the original
+    # positional item in the pattern. in this case,
+    # we're always matching "person=foo".
+    args_keys = PATTERN_ARGS_RE.findall(pattern_str)
+    kwargs = dict(zip(args_keys,args))
+    person = kwargs['person']
+
+    # if you don't need this behavior this, you can literally
+    # just use `person=args[0]`, since in this contrived case,
+    # you know you only ever have one arg, and it is always
+    # `person`.
 
     r = Random()
     is_werewolf = r.choice([True, False])
     return {
       'is_werewolf': is_werewolf,
-      'name': name,
+      'name': person,
       'plaintxt': '%s is%s a werewolf' % (
-          name,
+          person,
           " not" if is_werewolf else ""
       )
     }
